5adc403901006c8869569d7714dec59fbc81d0a2
alessanderleite <alessander.logistica@gmail.com>
2018-10-19 22:25:39 -0300

    creating a method startQuiz()

--- app/src/main/java/com/example/alessander/myawesomequiz/StartingScreenActivity.java
+++ app/src/main/java/com/example/alessander/myawesomequiz/StartingScreenActivity.java
@@ -1,7 +1,10 @@
 package com.example.alessander.myawesomequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class StartingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
     }
+
+    private void startQuiz() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
+
 }
